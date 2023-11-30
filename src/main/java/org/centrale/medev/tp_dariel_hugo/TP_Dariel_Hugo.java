d703bf979c3260f0ed38b79b5eac025b8fecd56a
Dariel Bezerra <darielbezerra13@gmail.com>
2023-11-30 10:47:16 +0100

    Commit

--- src/main/java/org/centrale/medev/tp_dariel_hugo/TP_Dariel_Hugo.java
+++ src/main/java/org/centrale/medev/tp_dariel_hugo/TP_Dariel_Hugo.java
@@ -4,6 +4,12 @@
 
 package org.centrale.medev.tp_dariel_hugo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 
 /**
  *
@@ -16,8 +22,55 @@ public class TP_Dariel_Hugo {
         /**
          * Lecture des images PGM
          */
+        
+        
+      String filePath = "/baboon.pgm";
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+            // Ler o cabeçalho do arquivo PGM
+            String magicNumber = br.readLine();
+            if (!magicNumber.equals("P2")) {
+                System.out.println("Format invalide");
+                br.close();
+                return;
+            }
+
+            int width = 0, height = 0, maxPixelValue = 0;
+
+            // Ignores les lignes des commentaires
+            String line;
+            while ((line = br.readLine()) != null && line.startsWith("#")) {
+            }
+
+            // Ler largura, altura e valor máximo do pixel
+            String[] dimensions = line.split("\\s");
+            width = Integer.parseInt(dimensions[0]);
+            height = Integer.parseInt(dimensions[1]);
+
+            maxPixelValue = Integer.parseInt(br.readLine());
+
+            // Ler os pixels da imagem
+            int[][] pixels = new int[height][width];
+            String[] pixel = new String[width];
+            
+            for (int i = 0; i < height; i++) {
+                br.readLine();
+                pixel = line.split("\\s");
+                for (int j = 0; j < width; j++) {
+                    pixels[i][j] = Integer.parseInt(pixel[j]);
+                }
+            }
+
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
+    
+    
+    
 
-}
+  }
