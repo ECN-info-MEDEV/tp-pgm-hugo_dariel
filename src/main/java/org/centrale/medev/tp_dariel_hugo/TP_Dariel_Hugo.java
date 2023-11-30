f2bbcb177bac83d16a44e393dfb2f52f33119b0e
Dariel Bezerra <darielbezerra13@gmail.com>
2023-11-30 09:56:32 +0100

    Commit

---
+++ src/main/java/org/centrale/medev/tp_dariel_hugo/TP_Dariel_Hugo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev.tp_dariel_hugo;
+
+/**
+ *
+ * @author darielbezerra
+ */
+public class TP_Dariel_Hugo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
