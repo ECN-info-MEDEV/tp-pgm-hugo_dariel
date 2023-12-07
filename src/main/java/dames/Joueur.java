f1339d03ce9b0f5bd8c1d0534a2a0ebc11ce864b
Moufid CHALAK <144018760+mchalak2023@users.noreply.github.com>
2023-12-07 11:08:31 +0100

    nbPieces

--- src/main/java/dames/Joueur.java
+++ src/main/java/dames/Joueur.java
@@ -3,15 +3,22 @@ package dames;
 import java.util.ArrayList;
 
 public class Joueur {
+    private String nom;
+    private ArrayList<Piece> pieces;
+    private int nbPieces;
     public String getNom() {
         return nom;
+    }
+    public int getNbPieces() {
+        return nbPieces;
+    }
+    public void setNbPieces(int nbPieces) {
+        this.nbPieces=nbPieces;
     }
 
     public ArrayList<Piece> getPieces() {
         return pieces;
     }
-
-    private String nom;
 
     public void setNom(String nom) {
         this.nom = nom;
@@ -21,11 +28,12 @@ public class Joueur {
         this.pieces = pieces;
     }
 
-    private ArrayList<Piece> pieces;
     public Joueur(String nom, ArrayList<Piece> pieces){
         this.nom=nom;
         this.pieces=new ArrayList<Piece>();
         this.pieces=pieces;
+        nbPieces=pieces.size();
+
 
     }
 
