16f655c56174c72bb639fdc76f54d251ddc943ff
Dariel Bezerra <darielbezerra13@gmail.com>
2023-12-14 08:28:11 +0100

    Fix errors

--- src/main/java/dames/Joueur.java
+++ src/main/java/dames/Joueur.java
@@ -1,34 +1,53 @@
-<<<<<<< HEAD
+
 package dames;
 
 import java.util.ArrayList;
 
+import dames.Piece;
+
+
+//public class Joueur {
+//    public String getNom() {
+//        return nom;
+//    }
+//
+//    public ArrayList<Piece> getPieces() {
+//        return pieces;
+//    }
+//
+//    private String nom;
+//
+//    public void setNom(String nom) {
+//        this.nom = nom;
+//    }
+//
+//    public void setPieces(ArrayList<Piece> pieces) {
+//        this.pieces = pieces;
+//    }
+//
+//    private ArrayList<Piece> pieces;
+//    public Joueur(String nom, ArrayList<Piece> pieces){
+//        this.nom=nom;
+//        this.pieces=new ArrayList<Piece>();
+//        this.pieces=pieces;
+//
+//    }
+//    
+//    public Joueur(){
+//        this.nom="joueur";
+//        this.pieces=new ArrayList<Piece>();
+//        for(int i=0;i<12;i++){
+//        this.pieces.add(new Piece());}
+//
+//    }
+//
+//}
+
 public class Joueur {
-    public String getNom() {
-        return nom;
-    }
-
-    public ArrayList<Piece> getPieces() {
-        return pieces;
-    }
-
     private String nom;
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public void setPieces(ArrayList<Piece> pieces) {
-        this.pieces = pieces;
-    }
-
     private ArrayList<Piece> pieces;
-    public Joueur(String nom, ArrayList<Piece> pieces){
-        this.nom=nom;
-        this.pieces=new ArrayList<Piece>();
-        this.pieces=pieces;
-
-    }
+    private int nbPieces;
+    
     
     public Joueur(){
         this.nom="joueur";
@@ -37,17 +56,13 @@ public class Joueur {
         this.pieces.add(new Piece());}
 
     }
-
-}
-=======
-package dames;
-
-import java.util.ArrayList;
-
-public class Joueur {
-    private String nom;
-    private ArrayList<Piece> pieces;
-    private int nbPieces;
+    public Joueur(String nom, ArrayList<Piece> pieces) {
+        this.nom = nom;
+        this.pieces = new ArrayList<Piece>();
+        this.pieces = pieces;
+        nbPieces = pieces.size();
+    }
+    
     public String getNom() {
         return nom;
     }
@@ -69,15 +84,4 @@ public class Joueur {
     public void setPieces(ArrayList<Piece> pieces) {
         this.pieces = pieces;
     }
-
-    public Joueur(String nom, ArrayList<Piece> pieces){
-        this.nom=nom;
-        this.pieces=new ArrayList<Piece>();
-        this.pieces=pieces;
-        nbPieces=pieces.size();
-
-
-    }
-
 }
->>>>>>> origin/vitaum
--- src/main/java/dames/tableau.java
+++ src/main/java/dames/tableau.java
@@ -4,35 +4,36 @@
  */
 package dames;
 
+import java.util.Arrays;
+
 /**
  *
  * @author hugoc
  */
 public class tableau {
-    
+
     private Piece[][] grille;
-    
-    private Joueur joueur1,joueur2;
-    
-    public void CreerTableau(){
-        
+
+    private Joueur joueur1, joueur2;
+
+    public void CreerTableau() {
+
         this.grille = new Piece[8][8];
         this.joueur1 = new Joueur();
         this.joueur2 = new Joueur();
         int pos1 = 0;
         int pos2 = 0;
-        
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                if((i+j)%2 == 0 && i<3){
-                    
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0 && i < 3) {
+
                     this.grille[i][j] = this.joueur1.getPieces().get(pos1);
-                    this.joueur1.getPieces().get(pos1).getPos().setPosition(i,j);
+                    this.joueur1.getPieces().get(pos1).getPos().setPosition(i, j);
                     pos1++;
-                }
-                else if((i+j)%2 == 0 && i>4){
+                } else if ((i + j) % 2 == 0 && i > 4) {
                     this.grille[i][j] = this.joueur2.getPieces().get(pos2);
-                    this.joueur2.getPieces().get(pos2).getPos().setPosition(i,j);
+                    this.joueur2.getPieces().get(pos2).getPos().setPosition(i, j);
                     pos2++;
                 }
             }
@@ -40,121 +41,107 @@ public class tableau {
 
     }
 
-     public void affiche(Joueur j) {
-         if(j == this.joueur1){
-        int numRows = this.grille[0].length;
-        int numCols = this.grille.length;
+    public void affiche(Joueur j) {
+        if (j == this.joueur1) {
+            int numRows = this.grille[0].length;
+            int numCols = this.grille.length;
 
-    int maxNomeLength = 1; 
+            int maxNomeLength = 1;
 
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                if (this.grille[row][col] != null) {
-                    int nomeLength = this.grille[row][col].toString().length();
-                    maxNomeLength = Math.max(maxNomeLength, nomeLength);
+            for (int row = 0; row < numRows; row++) {
+                for (int col = 0; col < numCols; col++) {
+                    if (this.grille[row][col] != null) {
+                        int nomeLength = this.grille[row][col].toString().length();
+                        maxNomeLength = Math.max(maxNomeLength, nomeLength);
+                    }
                 }
             }
-        }
-        int cellSize = maxNomeLength + 2; 
-        for (int row = numRows - 1; row >= 0; row--) {
-            System.out.printf("%-" + maxNomeLength + "d  ", row); 
-            for (int col = 0; col < numCols; col++) {
-                if (this.grille[row][col] == null && (row+col)%2==1) {
-                    System.out.printf("%-" + cellSize + "s", " "); 
-                } 
-                else if(this.grille[row][col] == null && (row+col)%2==0) {
-                    System.out.printf("%-" + cellSize + "s", "0"); 
-                } 
-                else {
-                    String nome = this.grille[row][col].toString();
-                    System.out.printf("%-" + cellSize + "s", nome); 
+            int cellSize = maxNomeLength + 2;
+            for (int row = numRows - 1; row >= 0; row--) {
+                System.out.printf("%-" + maxNomeLength + "d  ", row);
+                for (int col = 0; col < numCols; col++) {
+                    if (this.grille[row][col] == null && (row + col) % 2 == 1) {
+                        System.out.printf("%-" + cellSize + "s", " ");
+                    } else if (this.grille[row][col] == null && (row + col) % 2 == 0) {
+                        System.out.printf("%-" + cellSize + "s", "0");
+                    } else {
+                        String nome = this.grille[row][col].toString();
+                        System.out.printf("%-" + cellSize + "s", nome);
+                    }
                 }
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.printf("%-" + cellSize + "s", "");
-        for (int col = 0; col < numCols; col++) {
-            System.out.printf("%-" + cellSize + "s", col);
-        }
-         }
-         else{
-             int numRows = this.grille[0].length;
-        int numCols = this.grille.length;
-
-    int maxNomeLength = 1; 
-
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                if (this.grille[row][col] != null) {
-                    int nomeLength = this.grille[row][col].toString().length();
-                    maxNomeLength = Math.max(maxNomeLength, nomeLength);
-                }
-            }
-        }
-        int cellSize = maxNomeLength + 2; 
-        for (int row = numRows - 1; row >= 0; row--) {
-            System.out.printf("%-" + maxNomeLength + "d  ", row); 
-            for (int col = 0; col < numCols; col++) {
-                if (this.grille[row][col] == null && (row+col)%2==1) {
-                    System.out.printf("%-" + cellSize + "s", " "); 
-                } 
-                else if(this.grille[row][col] == null && (row+col)%2==0) {
-                    System.out.printf("%-" + cellSize + "s", "0"); 
-                } 
-                else {
-                    String nome = this.grille[row][col].toString();
-                    System.out.printf("%-" + cellSize + "s", nome); 
-                }
+                System.out.println();
             }
             System.out.println();
-        }
-        System.out.println();
-        System.out.printf("%-" + cellSize + "s", "");
-        for (int col = 0; col < numCols; col++) {
-            System.out.printf("%-" + cellSize + "s", col);
-        }
-             
-         }
-     }
-<<<<<<< HEAD
-     
-     
-     public void manger(Joueur j, Piece p, Point2D posmanger){
-         j.getPieces().remove(this.grille[posmanger.getX()][posmanger.getY()]);
-         this.grille[posmanger.getX()][posmanger.getY()] = null;
-         deplacer(p,posmanger.getX(),posmanger.getY());
-     }
-     
-     public void deplacer(Piece p, int x, int y){
-         this.grille[p.getPos().getX()][p.getPos().getY()]= null;  
-         this.grille[x][y]=p;         
-     }
-     
-    public void turnJeu(Boolean turn){
-=======
-    public void turnJeu(Boolean turn,Joueur j){
->>>>>>> origin/vitaum
-        if(turn){
-            Boolean[] peutmanger = new Boolean[j.getnNbPieces()];
-            Arrays.fill(booleanArray, Boolean.FALSE);
-            for(Piece p: this.joueur1.getPieces()){
-<<<<<<< HEAD
-                
-        }
-        }
-=======
-                if(p.peutManger(this)[0]||p.peutManger(this)[1]||p.peutManger(this)[2]||p.peutManger(this)[3]){
-                    
+            System.out.printf("%-" + cellSize + "s", "");
+            for (int col = 0; col < numCols; col++) {
+                System.out.printf("%-" + cellSize + "s", col);
+            }
+        } else {
+            int numRows = this.grille[0].length;
+            int numCols = this.grille.length;
+
+            int maxNomeLength = 1;
+
+            for (int row = 0; row < numRows; row++) {
+                for (int col = 0; col < numCols; col++) {
+                    if (this.grille[row][col] != null) {
+                        int nomeLength = this.grille[row][col].toString().length();
+                        maxNomeLength = Math.max(maxNomeLength, nomeLength);
+                    }
                 }
+            }
+            int cellSize = maxNomeLength + 2;
+            for (int row = numRows - 1; row >= 0; row--) {
+                System.out.printf("%-" + maxNomeLength + "d  ", row);
+                for (int col = 0; col < numCols; col++) {
+                    if (this.grille[row][col] == null && (row + col) % 2 == 1) {
+                        System.out.printf("%-" + cellSize + "s", " ");
+                    } else if (this.grille[row][col] == null && (row + col) % 2 == 0) {
+                        System.out.printf("%-" + cellSize + "s", "0");
+                    } else {
+                        String nome = this.grille[row][col].toString();
+                        System.out.printf("%-" + cellSize + "s", nome);
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+            System.out.printf("%-" + cellSize + "s", "");
+            for (int col = 0; col < numCols; col++) {
+                System.out.printf("%-" + cellSize + "s", col);
+            }
+
         }
-        }
-        
     }
-    public Piece[][] getGrille(){
-        return this.grille;
+
+    public void manger(Joueur j, Piece p, Point2D posmanger) {
+        j.getPieces().remove(this.grille[posmanger.getX()][posmanger.getY()]);
+        this.grille[posmanger.getX()][posmanger.getY()] = null;
+        deplacer(p, posmanger.getX(), posmanger.getY());
+    }
+
+    public void deplacer(Piece p, int x, int y) {
+        this.grille[p.getPos().getX()][p.getPos().getY()] = null;
+        this.grille[x][y] = p;
     }
     
+    
+
+    public void turnJeu(Boolean turn, Joueur j) {
+        if (turn) {
+            Boolean[] peutmanger = new Boolean[j.getNbPieces()];
+            Arrays.fill(peutmanger, Boolean.FALSE);
+            for (Piece p : this.joueur1.getPieces()) {
+                if (p.peutManger(this)[0] || p.peutManger(this)[1] || p.peutManger(this)[2] || p.peutManger(this)[3]) {
+
+                }
+            }
+        }
+
+    }
+
+    public Piece[][] getGrille() {
+        return this.grille;
+    }
 
 }
->>>>>>> origin/vitaum
